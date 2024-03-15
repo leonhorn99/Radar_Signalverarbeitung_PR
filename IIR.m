208d3cf7a83eae4fb311d57eca900439aff82a51
leonhorn99 <145433803+leonhorn99@users.noreply.github.com>
2024-03-15 13:15:03 +0100

    filtercoeffs uploaded

--- IIR.m
+++ IIR.m
@@ -5,7 +5,7 @@ n = 1:10000;
 ystep = (1-cos(theta*2*pi*(n))) / (theta*2*pi*sin(theta*2*pi));
 
 
-offset = - max(ystep)/2;
-scaling_f = (2^15-1)/max(ystep)*2;
+offset = 0;...- max(ystep)/2;
+scaling_f = 1;...(2^15-1)/max(ystep)*2;
 
 plot(scaling_f*(offset + ystep));
